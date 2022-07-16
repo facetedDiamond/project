6e44186f98d5e5d01c198b5a0a1611541b3dae50
facetedDiamond <direxevil@gmail.com>
2022-07-16 04:23:20 +0300

    test
    test

---
+++ script.js
@@ -0,0 +1,25 @@
+"use strict";
+
+//alert('Hello coolboy');
+
+//const result = confirm("Are you here?");
+//console.log(result);
+ 
+
+//const answer = prompt("Are you 18+?", "18");
+//console.log (typeof(answer));
+
+//const answers = [];
+//answers[0] = promt
+
+let incr = 10,
+    decr = 10;
+++incr;
+--decr;
+console.log(incr);
+console.log(decr);
+
+const  isCheked = true;
+const  isClose = false;
+
+console.log(isCheked || isClose );
